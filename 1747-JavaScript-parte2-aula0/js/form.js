1c5267a10683d8cddd97c8a8352672a96c1305a2
christianpva <christianpaulva@gmail.com>
2020-06-01 15:04:01 -0300

    Add files via upload

---
+++ 1747-JavaScript-parte2-aula0/js/form.js
@@ -0,0 +1,36 @@
+var botonAdicionar = document.querySelector("#adicionar-paciente");
+
+botonAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adicionar");
+    
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value
+    
+    var tabla = document.querySelector("#tabla-pacientes");
+
+    pacienteTr = document.createElement("tr");     
+    nombreTd = document.createElement("td");
+    alturaTd = document.createElement("td");
+    pesoTd = document.createElement("td");
+    gorduraTd = document.createElement("td");
+    imcTd = document.createElement("td");
+
+    nombreTd.textContent = nombre;
+    alturaTd.textContent = altura;
+    pesoTd.textContent = peso;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calcularIMC(peso,altura);
+
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    tabla.appendChild(pacienteTr);
+
+});
